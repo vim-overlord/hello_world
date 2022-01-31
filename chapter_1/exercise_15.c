f228bef1bfcf86fc57d79ee8d0c56e4d97bbaeef
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-01-30 21:04:22 -0800

    exercise 1-15

--- chapter_1/exercise_15.c
+++ chapter_1/exercise_15.c
@@ -1,4 +1,8 @@
+/* Exercise 1-15. Rewrite the temperature conversion program of Section 1.2 to
+    use a function for conversion. */
 #include <stdio.h>
+
+float convert(float fahr);
 
 /* print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300; floating-point version */
@@ -11,10 +15,18 @@ main()
     upper = 300;    /* upper limit */
     step = 20;      /* step size */
 
+    /* print Fahrenheit-Celsius table */
     fahr = lower;
     while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
+        celsius = convert(fahr);
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+    return 0;
+}
+
+/* convert:  convert fahrenheit temperature to celsius */
+float convert(float fahr)
+{
+    return (5.0/9.0) * (fahr-32.0);
 }
