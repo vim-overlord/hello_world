c1959d906e8b705b0798fa78ee0a21e5eb76f0ef
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-02-17 22:59:34 -0800

    import getline from 1.9

---
+++ chapter_2/exercise_02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAXLINE 1000    /* maximum input line size */
+
+/* getline:  read a line into s, return length */
+int my_getline(char s[], int lim)
+{
+    int c, i;
+
+    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
