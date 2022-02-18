bf33cc91bf4b42af866ba6cbe5ad7420868c8dc5
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-02-17 23:24:39 -0800

    exercise 2-2

--- chapter_2/exercise_02.c
+++ chapter_2/exercise_02.c
@@ -1,14 +1,46 @@
+/* Exercise 2-2. Write a loop equivalent to the for loop above without using &&
+    or ||. */
 #include <stdio.h>
 
 #define MAXLINE 1000    /* maximum input line size */
 
+enum boolean { NO, YES };
+
+int my_getline(char line[], int maxline);
+
+/* testing code */
+main()
+{
+    int len;            /* current line length */
+    char line[MAXLINE]; /* current input line */
+
+    while ((len = my_getline(line, MAXLINE)) > 0) {
+        printf("%s", line);
+        printf("length: %d\n", len);
+    }
+    return 0;
+}
+
 /* getline:  read a line into s, return length */
 int my_getline(char s[], int lim)
 {
-    int c, i;
+    int c;
+    int i = 0;
+    int finished = NO;
 
-    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-        s[i] = c;
+    while (!finished) {
+        if (i < lim - 1) {
+            if ((c = getchar()) != EOF) {
+                if (c != '\n') {
+                    s[i] = c;
+                    ++i;
+                } else
+                    finished = YES;
+            } else
+                finished = YES;
+        } else
+            finished = YES;
+    }
     if (c == '\n') {
         s[i] = c;
         ++i;
