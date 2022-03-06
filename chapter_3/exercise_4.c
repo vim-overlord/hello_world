d2602afc19de1d7e4e3b36949841adab12c8491d
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-03-05 17:28:34 -0800

    exercise 3-4

--- chapter_3/exercise_4.c
+++ chapter_3/exercise_4.c
@@ -1,7 +1,23 @@
+/* Exercise 3-4. In a two’s complement number representation, our version of
+    itoa does not handle the largest negative number, that is, the value of n
+    equal to −(2^(wordsize−1)). Explain why not. Modify it to print that value
+    correctly, regardless of the machine on which it runs. */
+#include <stdio.h>
+#include <limits.h>
 #include <string.h>
 
 void reverse(char s[]);
 void itoa(int n, char s[]);
+
+/* testing code */
+main()
+{
+    char s[12];
+
+    itoa(INT_MIN, s);
+    printf("%s\n", s);
+    return 0;
+}
 
 /* reverse:  reverse string s in place */
 void reverse(char s[])
@@ -20,13 +36,12 @@ void itoa(int n, char s[])
 {
     int i, sign;
 
-    if ((sign = n) < 0)  /* record sign */
-        n = -n;          /* make n positive */
+    sign = (n >= 0) ? 1 : -1;
     i = 0;
     do {       /* generate digits in reverse order */
-        s[i++] = n % 10 + '0';   /* get next digit */
-    } while ((n /= 10) > 0);     /* delete it */
-    if (sign < 0)
+        s[i++] = n % 10 * sign + '0';   /* get next digit */
+    } while (sign * (n /= 10) > 0);     /* delete it */
+    if (sign == -1)
         s[i++] = '-';
     s[i] = '\0';
     reverse(s);
