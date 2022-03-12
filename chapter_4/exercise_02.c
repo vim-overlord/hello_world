7ab8548baaa48d280857e4782864d6a4786f13e9
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-03-12 12:50:27 -0800

    exercise 4-2

--- chapter_4/exercise_02.c
+++ chapter_4/exercise_02.c
@@ -1,10 +1,43 @@
+/* Exercise 4-2. Extend atof to handle scientific notation of the form
+    123.45e-6 where a floating-point number may be followed by e or E and an
+    optionally signed exponent. */
+#include <stdio.h>
 #include <ctype.h>
+
+#define MAXLINE 1000    /* maximum input line length */
+
+int my_getline(char line[], int max);
+double atof(char s[]);
+
+/* testing code */
+main()
+{
+    char line[MAXLINE];
+
+    my_getline(line, MAXLINE);
+    printf("%f\n", atof(line));
+    return 0;
+}
+
+/* getline:  get line into s, return length */
+int my_getline(char s[], int lim)
+{
+    int c, i;
+
+    i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
 
 /* atof:  convert string s to double */
 double atof(char s[])
 {
     double val, power;
-    int i, sign;
+    int i, sign, exp, exp_sign;
 
     for (i = 0; isspace(s[i]); i++)  /* skip white space */
         ;
@@ -18,6 +51,19 @@ double atof(char s[])
     for (power = 1.0; isdigit(s[i]); i++) {
         val = 10.0 * val + (s[i] - '0');
         power *= 10.0;
+    }
+    if (tolower(s[i]) == 'e') {
+        ++i;
+        exp_sign = (s[i] == '-') ? -1 : 1;
+        if (s[i] == '+' || s[i] == '-')
+            ++i;
+        for (exp = 0; isdigit(s[i]); ++i)
+            exp = 10 * exp + (s[i] - '0');
+        exp *= exp_sign;
+        for (; exp < 0; ++exp)
+            power *= 10.0;
+        for (; exp > 0; --exp)
+            power /= 10.0;
     }
     return sign * val / power;
 }
