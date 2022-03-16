9bddf1347ad0cada957c5d1ea723e21dbfde8592
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-03-15 23:30:59 -0700

    exercise 4-12

--- chapter_4/exercise_12.c
+++ chapter_4/exercise_12.c
@@ -1,13 +1,29 @@
+/* Exercise 4-12. Adapt the ideas of printd to write a recursive version of
+    itoa; that is, convert an integer into a string by calling a recursive
+    routine. */
 #include <stdio.h>
 
-/* printd:  print n in decimal */
-void printd(int n)
+void itoa(int n, char s[]);
+
+/* testing code */
+main()
 {
-    if (n < 0) {
-        putchar('-');
-        n = -n;
-    }
+    char s[12];
+
+    itoa(-100, s);
+    printf("%s\n", s);
+    return 0;
+}
+
+/* itoa:  convert n to characters in s */
+void itoa(int n, char s[])
+{
+    static int i = 0;
+
     if (n / 10)
-        printd(n / 10);
-    putchar(n % 10 + '0');
+        itoa(n / 10, s);
+    else if (n < 0)
+        s[i++] = '-';
+    s[i++] = ((n >= 0) ? n : -n) % 10 + '0';
+    s[i] = '\0';
 }
