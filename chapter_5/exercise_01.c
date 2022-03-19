abea4215e2eaaefbd336419527b466ba0d691996
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-03-18 21:02:16 -0700

    exercise 5-1

--- chapter_5/exercise_01.c
+++ chapter_5/exercise_01.c
@@ -1,3 +1,6 @@
+/* Exercise 5-1. As written, getint treats a + or - not followed by a digit as
+    a valid representation of zero. Fix it to push such a character back on the
+    input. */
 #include <ctype.h>
 #include <stdio.h>
 
@@ -18,7 +21,7 @@ main()
         if (temp != 0)
             printf("%d\n", n);
         else {
-            printf("Error: not a number\n");
+            printf("Error: not an integer\n");
             break;
         }
     }
@@ -52,6 +55,11 @@ int getint(int *pn)
     sign = (c == '-') ? -1 : 1;
     if (c == '+' || c == '-')
         c = getch();
+    if (!isdigit(c) && c != EOF) {
+        ungetch(c);
+        ungetch(c); /* unget sign */
+        return 0;
+    }
     for (*pn = 0; isdigit(c); c = getch())
         *pn = 10 * *pn + (c - '0');
     *pn *= sign;
