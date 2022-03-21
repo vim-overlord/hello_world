114b874d74ed0d5e971472c4a001397deecb77ba
Avaneeth Kumar <avaneethkumar07@gmail.com>
2022-03-20 17:04:47 -0700

    exercise 5-6 (getline and atof)

--- chapter_5/exercise_06_1.c
+++ chapter_5/exercise_06_1.c
@@ -1,10 +1,13 @@
+/* Exercise 5-6. Rewrite appropriate programs from earlier chapters and
+    exercises with pointers instead of array indexing. Good possibilities
+    include getline (Chapters 1 and 4), atoi... */
 #include <stdio.h>
 #include <ctype.h>
 
 #define MAXLINE 1000    /* maximum input line length */
 
-int my_getline(char [], int);
-double atof(char []);
+int my_getline(char *, int);
+double atof(char *);
 
 main()
 {
@@ -17,36 +20,36 @@ main()
 }
 
 /* getline:  get line into s, return length */
-int my_getline(char s[], int lim)
+int my_getline(char *s, int lim)
 {
-    int c, i;
+    int c;
+    char *p = s;
 
-    i = 0;
-    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
-        s[i++] = c;
+    for (; lim > 0 && (c = getchar()) != EOF && c != '\n'; ++s, --lim)
+        *s = c;
     if (c == '\n')
-        s[i++] = c;
-    s[i] = '\0';
-    return i;
+        *s++ = c;
+    *s = '\0';
+    return s - p;
 }
 
 /* atof:  convert string s to double */
-double atof(char s[])
+double atof(char *s)
 {
     double val, power;
-    int i, sign;
+    int sign;
 
-    for (i = 0; isspace(s[i]); i++)  /* skip white space */
-        ;
-    sign = (s[i] == '-') ? -1 : 1;
-    if (s[i] == '+' || s[i] == '-')
-        i++;
-    for (val = 0.0; isdigit(s[i]); i++)
-        val = 10.0 * val + (s[i] - '0');
-    if (s[i] == '.')
-        i++;
-    for (power = 1.0; isdigit(s[i]); i++) {
-        val = 10.0 * val + (s[i] - '0');
+    while (isspace(*s)) /* skip white space */
+        ++s;
+    sign = (*s == '-') ? -1 : 1;
+    if (*s == '+' || *s == '-')
+        ++s;
+    for (val = 0.0; isdigit(*s); ++s)
+        val = 10.0 * val + (*s - '0');
+    if (*s == '.')
+        ++s;
+    for (power = 1.0; isdigit(*s); ++s) {
+        val = 10.0 * val + (*s - '0');
         power *= 10.0;
     }
     return sign * val / power;
